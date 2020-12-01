78c3fe55e6e1a5bf723d4d414e723459fb21b6aa
Ruben <rubn.vp@gmail.com>
2020-12-01 19:05:35 +0100

    day 1

---
+++ base-main.js
@@ -0,0 +1,16 @@
+var inputText = `
+
+`;
+
+function parseInput(inputText) {
+    return inputText;
+}
+
+function resolve1(input) {
+    return input;
+}
+
+const input = parseInput(inputText);
+const output = resolve1(input);
+
+console.log(output);
