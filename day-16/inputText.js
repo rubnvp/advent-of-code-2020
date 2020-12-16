95349cb31e99255530b1a7c5e2b92b7b36a5c95c
rubnvp <ruben.valseca@clarity.ai>
2020-12-16 18:33:27 +0100

    day 16 base

---
+++ day-16/inputText.js
@@ -0,0 +1,3 @@
+var inputText = `
+base input
+`;
---
+++ day-16/main.js
@@ -0,0 +1,20 @@
+var inputText = `
+ola
+`;
+
+function parseInput(inputText) {
+    return inputText;
+}
+
+function resolve1(input) {
+    return input;
+}
+
+function resolve2(input) {
+    return input;
+}
+
+const input = parseInput(inputText);
+const output = resolve1(input);
+
+console.log(output);
